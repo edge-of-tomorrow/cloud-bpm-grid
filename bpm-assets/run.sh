d018792aeebe1111d27928038d02eba36ee92758
Ivo Bek <ibek@redhat.com>
2016-10-03 14:33:37 +0200

    Fixed integration between business-central and process-server

--- bpm-assets/run.sh
+++ bpm-assets/run.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # Clone repositories which should be added to Business Central
+# Optionally you can add hooks to automatically push commits to the origin repositories
 
-INIT_DIR=init
-if [ ! -d "$INIT_DIR" ]; then
-    mkdir -m 777 $INIT_DIR
-    git clone git+ssh://code.engineering.redhat.com/bpms-qe-assets.git init/bpms-qe-assets
+NIOGIT_DIR=niogit
+if [ ! -d "$NIOGIT_DIR" ]; then
+    mkdir -m 777 $NIOGIT_DIR
+    git clone --mirror git+ssh://code.engineering.redhat.com/cloud-bpm-grid-system.git niogit/system.git
+    git clone --mirror git+ssh://code.engineering.redhat.com/bpms-qe-assets.git niogit/bpms-qe-assets.git
 else
     cd init/bpms-qe-assets
     git pull
--- business-central/run.sh
+++ business-central/run.sh
@@ -3,7 +3,7 @@
 # TODO: change --net="host" but we need to access the execution server (make it a docker image as well?)
 # then, add  --link artifact-repository:arepo
 
-BPM_ASSETS=/home/ibek/Work/github/cloud-bpm-grid/bpm-assets/init
+BPM_ASSETS=/home/ibek/Work/github/cloud-bpm-grid/bpm-assets/niogit
 if [ ! -d "$BPM_ASSETS" ]; then
     cd ../bpm-assets
     ./run.sh
@@ -11,4 +11,4 @@ if [ ! -d "$BPM_ASSETS" ]; then
 fi
 
 docker rm -f business-central
-docker run -d -p 8080:8080 --net="host" --name business-central -v $BPM_ASSETS:/init-repositories kie/business-central
+docker run -d -p 8080:8080 --net="host" --name business-central -v $BPM_ASSETS:/opt/jboss/wildfly/bin/.niogit kie/business-central
